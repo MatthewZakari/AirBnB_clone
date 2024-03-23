ffd1f5fe1f789edc0cac6b87d3998c9ca0a2f27f
Matthew Zakari <zakarimatthew62@gmail.com>
2024-03-23 19:51:49 +0000

    pycodestyle correction

--- models/base_model.py
+++ models/base_model.py
@@ -5,6 +5,7 @@ BaseModel Module
 import uuid
 from datetime import datetime
 
+
 class BaseModel:
     """
     BaseModel class
@@ -33,17 +34,18 @@ class BaseModel:
         """
         Returns string representation of the object
         """
-        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+        return "[{}] ({}) {}".format(self.__class__.__name__,
+                self.id, self.__dict__)
 
     def save(self):
         """
-        Updates the public instance attribute updated_at with the current datetime
+        Updates pub instance attribute updated_at with the current datetime
         """
         self.updated_at = datetime.now()
 
     def to_dict(self):
         """
-        Returns a dictionary containing all keys/values of __dict__ of the instance
+        Returns dict. containing all keys/values of __dict__ of the instance
         """
         obj_dict = self.__dict__.copy()
         obj_dict['__class__'] = self.__class__.__name__
